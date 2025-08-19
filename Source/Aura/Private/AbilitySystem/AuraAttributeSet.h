7bb455ab40f20d17d931339bc372f30fc7e5711f
Renan Souza <renan.souza@powerpush.com.br>
2025-08-18 22:21:15 -0300

    mking replicated properties for attribute set

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.h
@@ -7,5 +7,23 @@
 UCLASS()
 class UAuraAttributeSet : public UAttributeSet {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(ReplicatedUsing=OnRep_Health)
+	FGameplayAttributeData Health;
 	
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+
+public:
+	UAuraAttributeSet();
+
+	// this function must be used to register all the replicated properties. -Renan
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 };
